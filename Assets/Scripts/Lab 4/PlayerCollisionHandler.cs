b5ee16ac08480d4059fcbe6556c8576da5558239
phanthanhhao0612 <phanthanhhao0612@gmail.com>
2026-02-06 14:46:28 +0700

    update lab 4

---
+++ Assets/Scripts/Lab 4/PlayerCollisionHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Va chạm (Collision) với: " + collision.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Đi vào Trigger với: " + other.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/Lab 4/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector2(x, y).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * speed;
+    }
+}
